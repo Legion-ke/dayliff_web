8a513679b3474f5ba077aa2f1d6e8a47aa5e0e33
Josphat Mwangi <josphatwanjiruw@gmail.com>
2023-11-01 21:49:35 +0300

    create component for piechart and bargraph

---
+++ src/common/bargraph.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import { BarChart } from "@mui/x-charts/BarChart";
+
+export default function BarGraph({ pData, label, xLabels }) {
+  return (
+    <BarChart
+      width={700}
+      height={400}
+      series={[
+        {
+          data: pData,
+          label: label,
+          id: "pvId",
+          stack: "total",
+        },
+      ]}
+      xAxis={[{ data: xLabels, scaleType: "band" }]}
+    />
+  );
+}
---
+++ src/common/piegraph.jsx
@@ -0,0 +1,27 @@
+import { PieChart, pieArcClasses } from "@mui/x-charts/PieChart";
+
+const data = [
+  { id: 0, value: 10, label: "OnTime Deliver" },
+  { id: 1, value: 15, label: "Late Deliver" },
+  { id: 2, value: 20, label: "Partial deliver" },
+];
+
+export default function PieGraph() {
+  return (
+    <PieChart
+      series={[
+        {
+          data,
+          highlightScope: { faded: "global", highlighted: "item" },
+          faded: { innerRadius: 30, additionalRadius: -30 },
+        },
+      ]}
+      sx={{
+        [`& .${pieArcClasses.faded}`]: {
+          fill: "gray",
+        },
+      }}
+      height={400}
+    />
+  );
+}
